b917ff31cd63080acc61137de99aa00bfd20180c
9William <gwilliam@student.telkomuniversity.ac.id>
2023-04-14 17:06:38 +0700

    Bad Smell Duplicate Code
    Menghilangkan duplikasi pada kode isForeigner

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -95,20 +95,14 @@ public class Employee extends EmployeeDetails{
     public void setMonthlySalary(int grade) {
         if (grade == 1) {
             monthlySalary = 3000000;
-            if (isForeigner) {
-                monthlySalary = (int) (3000000 * 1.5);
-            }
         } else if (grade == 2) {
             monthlySalary = 5000000;
-            if (isForeigner) {
-                monthlySalary = (int) (3000000 * 1.5);
-            }
         } else if (grade == 3) {
             monthlySalary = 7000000;
-            if (isForeigner) {
+        }
+        if (isForeigner) {
                 monthlySalary = (int) (3000000 * 1.5);
             }
-        }
     }
 
     public void setAnnualDeductible(int deductible) {
